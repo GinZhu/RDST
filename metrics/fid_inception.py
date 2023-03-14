376db2769aa61afe8752acb900dfffd0ea294318
Jin Zhu <zhujin1121@gmail.com>
2023-03-14 11:55:23 +0800

    config files

--- metrics/fid_inception.py
+++ metrics/fid_inception.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 import torchvision
 from os.path import exists
 
-from torchvision.models.utils import load_state_dict_from_url
-
+# from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 # Inception weights ported to Pytorch from
 # http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz
 FID_WEIGHTS_URL = 'https://github.com/mseitzer/pytorch-fid/releases/download/fid_weights/pt_inception-2015-12-05-6726825d.pth'
